270a5e0b9ce52fc19195425b9b4008f7ccac2d9a
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2024-03-06 18:10:24 +0100

    Add tool for syncing app commands and integrate in cicd (#4)

---
+++ cmd/dslash/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	var token = os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		log.Fatal("Environment variable DISCORD_TOKEN is unset")
+	}
+
+	var appID = os.Getenv("DISCORD_APPLICATION_ID")
+	if appID == "" {
+		log.Fatal("Environment variable DISCORD_APPLICATION_ID is unset")
+	}
+
+	var specPath = "application-commands.json"
+	if len(os.Args) > 1 {
+		specPath = os.Args[1]
+	}
+
+	f, err := os.Open(specPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var commands []*discordgo.ApplicationCommand
+
+	decoder := json.NewDecoder(f)
+	decoder.DisallowUnknownFields()
+
+	err = decoder.Decode(&commands)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createdCommands, err := s.ApplicationCommandBulkOverwrite(appID, "", commands)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The following commands have been created/updated:\n\n")
+	for _, cmd := range createdCommands {
+		fmt.Printf("  - %s\n", cmd.Name)
+	}
+}
